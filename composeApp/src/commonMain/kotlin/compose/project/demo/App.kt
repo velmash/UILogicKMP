4c345af7ce3e38473b5dfceafa8bb2aadcd4a540
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-12-13 17:07:45 +0900

    :wrench: Timezone Binding  config

--- composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,7 +21,9 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 import composedemo.composeapp.generated.resources.Res
 import composedemo.composeapp.generated.resources.compose_multiplatform
 import kotlinx.datetime.Clock
+import kotlinx.datetime.IllegalTimeZoneException
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
@@ -28,13 +31,28 @@ import kotlinx.datetime.toLocalDateTime
 @Preview
 fun App() {
     MaterialTheme {
+        var location by remember { mutableStateOf("Europe/Paris") }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
 
         Column {
             Text(timeAtLocation)
-            Button(onClick = { timeAtLocation = "13:30" }) {
+            TextField(value = location, onValueChange = { location = it })
+            Button(onClick = { timeAtLocation = currentTimeAt(location) ?: "Invalid Location" }) {
                 Text("Show Time At Location")
             }
         }
+    }
+}
+
+fun currentTimeAt(location: String): String? {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    return try {
+        val time = Clock.System.now()
+        val zone = TimeZone.of(location)
+        val localTime = time.toLocalDateTime(zone).time
+        "The time in $location is ${localTime.formatted()}"
+    } catch (ex: IllegalTimeZoneException) {
+        null
     }
 }
