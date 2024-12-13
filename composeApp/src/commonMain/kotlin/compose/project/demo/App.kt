d2a44a8ba0ad83f830f81fa8485612bcb41a0b0b
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-12-13 16:59:35 +0900

    :wrench: Local Time App Configure / Desktop App Size Configure

--- composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
+++ composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -28,32 +28,13 @@ import kotlinx.datetime.toLocalDateTime
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = "Today's date is ${todaysDate()}",
-                modifier = Modifier.padding(20.dp),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center
-            )
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        var timeAtLocation by remember { mutableStateOf("No location selected") }
+
+        Column {
+            Text(timeAtLocation)
+            Button(onClick = { timeAtLocation = "13:30" }) {
+                Text("Show Time At Location")
             }
         }
     }
-}
-
-fun todaysDate(): String {
-    fun LocalDateTime.format() = toString().substringBefore('T')
-
-    val now = Clock.System.now()
-    val zone = TimeZone.currentSystemDefault()
-    return now.toLocalDateTime(zone).format()
 }
